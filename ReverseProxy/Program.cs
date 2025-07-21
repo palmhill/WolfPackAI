8de3fa2d3a2771245d36ceca2f91b9e95769f108
Edgett Hilimire <edgett@palmhillai.com>
2025-07-21 18:27:45 -0400

    more progress

--- ReverseProxy/Program.cs
+++ ReverseProxy/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Yarp.ReverseProxy.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,23 +8,11 @@ builder.AddServiceDefaults();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-builder.Services.AddAuthentication("AzureAd")
-    .AddJwtBearer("AzureAd", options =>
-    {
-        options.Authority = $"https://login.microsoftonline.com/{builder.Configuration["AzureAd:TenantId"]}/v2.0";
-        options.Audience = builder.Configuration["AzureAd:ClientId"];
-        options.TokenValidationParameters.ValidateIssuer = true;
-    });
-
-builder.Services.AddAuthorization();
-
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
 // Configure pipeline
-app.UseAuthentication();
-app.UseAuthorization();
 app.MapReverseProxy();
 
 app.Run();
